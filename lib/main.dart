b96ee8fee83dc3d9906bd07c10bccce7d583e0a2
Ronaldo <ronaldovctor@outlook.com>
2023-08-21 19:33:33 -0300

    little changes

--- lib/main.dart
+++ lib/main.dart
@@ -106,11 +106,12 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Times the button has been pressed:',
+              style: TextStyle(color: Colors.purpleAccent, fontSize: 18),
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.headlineLarge,
             ),
           ],
         ),
@@ -118,7 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.arrow_upward_outlined),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
